0a3115003a84bfef4f5cca04d6160766f790425d
Justin Bond <bond.justink@gmail.com>
2019-06-22 12:45:02 -0700

    added tween to default layout to prevent initial load flicker

--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Helmet from 'react-helmet';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { TweenMax } from 'gsap';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
@@ -22,8 +23,14 @@ const DefaultLayout = (props) => {
 
   library.add(fab);
 
+  let defaultLayout = null;
+
+  useEffect(() => {
+    TweenMax.to(defaultLayout, 1, { opacity: 1 });
+  }, []);
+
   return (
-    <div className={rootClassnames}>
+    <div className={rootClassnames} ref={(node) => { defaultLayout = node; }}>
       <Helmet defaultTitle={'Justin Bond'} titleTemplate={'%s | Justin Bond'}>
         <html lang={'en'} />
         <meta name={'description'} content={'Hello, I‘m Justin Bond and I am a full-stack web developer from Costa Mesa, California.'} />
