90d11d77e4c74d296534d6a951b5fb62f37594f1
Justin Bond <bond.justink@gmail.com>
2020-05-08 16:21:58 -0700

    Home :: update intro copy

--- src/components/HomeIntro.js
+++ src/components/HomeIntro.js
@@ -23,14 +23,14 @@ const HomeIntro = () => {
   return (
     <div className={rootClassnames}>
       <h1 className={`${ns}__text`} ref={(node) => { homeIntroText = node; }}>
-        Hello, I‘m Justin Bond and I am a Front-End Web Developer in Orange County, CA.
+        Hello, I‘m Justin Bond and I am a Front-End Engineer based out of Orange County, CA.
       </h1>
       <ul className={`${ns}__bullets`}>
         <li ref={(node) => { homeIntroBullets[0] = node; }}>
-          Skilled in Wordpress, Magento, Shopify, React, PHP, and Javascript.
+          Skilled in React/Gatsby, Shopify, Magento, Wordpress, MySQL, GraphQL, PHP, and Javascript.
         </li>
         <li ref={(node) => { homeIntroBullets[1] = node; }}>
-          When I am not coding I enjoy listening to music, watching football, golfing, riding motorcycles, and going on hikes with my fur child.
+          When I am not coding I enjoy listening to music, watching football, golfing, riding motorcycles, and going on hikes with my wife and our fur child.
         </li>
       </ul>
     </div>
